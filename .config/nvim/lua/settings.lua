7a7e45c644f1e94ed90059858036759a62ea6ace
trebinor <trebinor@users.noreply.github.com>
2022-01-02 17:35:56 -0500

    Sensible nvim defaults.

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -1,7 +1,13 @@
--- global settings
-vim.o.modelines=0
+local opt = vim.opt             -- options table
 
--- buffer-local settings
-vim.bo.expandtab=true
-vim.bo.shiftwidth=2
-vim.bo.softtabstop=2
+opt.modeline = false            -- prevent security vulnerability (FIXME: Is this still needed for Neovim?)
+opt.modelines = 0
+opt.expandtab = true            -- convert tabs to spaces
+opt.tabstop = 4                 -- spaces per tab
+opt.shiftwidth = 4              -- indent size
+opt.softtabstop = 0             -- use shiftwidth value
+opt.list = true                 -- invisible characters
+opt.number = true               -- line numbers
+opt.relativenumber = true       -- relative line numbers
+opt.wrap = false                -- line wrap
+opt.autoindent = true           -- Indent next line at current line level
