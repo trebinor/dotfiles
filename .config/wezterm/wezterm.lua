e4909bb971a2970ddd1ce7a29b950276c81325c7
trebinor <trebinor@users.noreply.github.com>
2023-10-31 12:03:10 -0700

    Default branch is now main.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,9 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
-config.font = wezterm.font 'FiraCode Nerd Font'
+config.font = wezterm.font("FiraCode Nerd Font")
+config.allow_win32_input_mode = true
 return config
